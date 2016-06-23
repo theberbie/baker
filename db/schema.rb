357f5629b6dbd7aaad7b90353e860c8a50eeea94
vagrant <vagrant@precise32.(none)>
2016-06-23 19:02:56 +0000

    Added input fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623041222) do
+ActiveRecord::Schema.define(version: 20160623183911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20160623041222) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
+    t.integer  "phone_number"
+    t.datetime "date_of_birth"
+    t.string   "favorite_color"
+    t.string   "favorite_song"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
