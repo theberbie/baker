76ead544a3ecb6bb24611fce8967ef7307300349
vagrant <vagrant@precise32.(none)>
2016-07-06 23:03:36 +0000

    Overriding Devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+def new
+	super
+end
 
+def create
+	@user = User.new(params[:user])
+flash[:notice]= "yay"
+redirect_to new_artisan_product_path
+render :action => :new
+end
 
 def resource_params
   params.require(resource_name).permit(:email, :password, :password_confirmation)
