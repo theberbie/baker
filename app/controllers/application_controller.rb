ba41bb7ee57f896ffe592edeb3ae961a1b14c2b1
vagrant <ahbabousara@gmail.com>
2016-07-07 04:01:09 +0000

    Overrode Devise Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+
   protected
 
    def configure_permitted_parameters
--- app/controllers/sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class SessionsController < Devise::SessionsController
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || account_url
-  end
-end
